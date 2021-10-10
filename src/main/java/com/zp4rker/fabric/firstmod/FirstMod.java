f1f4a38b64b0c80b1db1e8363a50f4569473359f
zp4rker <iam@zp4rker.com>
2021-10-11 10:33:27 +1100

    created project

---
+++ src/main/java/com/zp4rker/fabric/firstmod/FirstMod.java
@@ -0,0 +1,13 @@
+package com.zp4rker.fabric.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * @author zp4rker
+ */
+public class FirstMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Mod loaded!");
+    }
+}
